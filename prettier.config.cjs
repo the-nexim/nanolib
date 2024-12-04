62e003d03f69e4136a4ac4783fbee3fff126b6d9
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-04 13:01:31 +0330

    chore: base config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,6 +2,5 @@
  * @type {import('prettier').Config}
  */
 module.exports = {
-  ...require('@alwatr/prettier-config'),
-  plugins: [require.resolve('prettier-plugin-tailwindcss'), require.resolve('prettier-plugin-packagejson')],
+  ...require('@nexim/prettier-config'),
 };
