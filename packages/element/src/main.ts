5a32287ddac6706412a6e2e65fc86043fe3bb778
arashagp <arash.qardashpoor@gmail.com>
2024-12-04 11:56:43 +0330

    feat(element): export additional modules from main entry point

--- packages/element/src/main.ts
+++ packages/element/src/main.ts
@@ -1,3 +1,9 @@
 import {packageTracer} from '@alwatr/package-tracer';
 
+export * from './lib/base-element.js';
+export * from './lib/lit-light-dom-style.js';
+export * from './mixin/light-dom.js';
+export * from './mixin/logging.js';
+
+
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
