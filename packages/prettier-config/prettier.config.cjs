8e071fa4d2585458fe588afe967d54a5eb96b437
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-04 12:41:10 +0330

    feat(prettier-config): copy package from @alwatr/tsconfig-base
    Co-authored-by: S. Ali Mihandoost <ali@mihandoost.com>

---
+++ packages/prettier-config/prettier.config.cjs
@@ -0,0 +1,16 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 140,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  tabWidth: 2,
+  plugins: [require.resolve('prettier-plugin-tailwindcss'), require.resolve('prettier-plugin-packagejson')],
+};
