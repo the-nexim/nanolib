7532dc738b8d8e66c9ee12c75485dd30ca32a897
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-07 08:44:16 +0330

    refactor(element/light-dom): move style utils to class as static property

--- packages/element/src/main.ts
+++ packages/element/src/main.ts
@@ -1,8 +1,6 @@
 import {packageTracer} from '@alwatr/package-tracer';
 
-export * from './lib/base-element.js';
-export * from './lib/lit-light-dom-style.js';
+__dev_mode__: packageTracer.add(__package_name__, __package_version__);
+
 export * from './mixin/light-dom.js';
 export * from './mixin/logging.js';
-
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
--- packages/element/src/mixin/light-dom.ts
+++ packages/element/src/mixin/light-dom.ts
@@ -1,10 +1,33 @@
-import {lightDomStyles} from '../lib/lit-light-dom-style.js';
-
-import type {Constructor} from '../lib/type.js';
-import type {LitElement} from 'lit';
+import type {Constructor} from '../type.js';
+import type {LitElement, CSSResultGroup, ReactiveElement} from 'lit';
 
 export function LightDomMixin<T extends Constructor<LitElement>>(superClass: T): T {
   class MixinClass extends superClass {
+    static flatCssText(styles?: CSSResultGroup): string {
+      if (styles === undefined) return '';
+      if ('cssText' in styles) return styles.cssText.trim();
+      if (Array.isArray(styles)) {
+        return styles
+          .map((style) => ('cssText' in style ? style.cssText : MixinClass.flatCssText(style)))
+          .join('\n')
+          .trim();
+      }
+      return '';
+    }
+
+    static lightDomStyles(tagName: string, element: typeof ReactiveElement): void {
+      const className = `${tagName}-light-dom-style`;
+      if (document.querySelector(`style.${className}`) !== null) return;
+
+      const cssText = MixinClass.flatCssText(element.styles);
+      if (cssText === '') return;
+
+      const styleEl = document.createElement('style');
+      styleEl.classList.add(className);
+      styleEl.innerHTML = cssText;
+      document.head.append(styleEl);
+    }
+
     // Render template into light DOM
     override createRenderRoot() {
       return this;
@@ -13,7 +36,7 @@ export function LightDomMixin<T extends Constructor<LitElement>>(superClass: T):
     // Add light dom class when the element is connected to the DOM
     override connectedCallback() {
       super.connectedCallback();
-      lightDomStyles(this.localName, this.constructor as typeof LitElement);
+      MixinClass.lightDomStyles(this.tagName.toLowerCase(), this.constructor as typeof LitElement);
     }
   }
 
