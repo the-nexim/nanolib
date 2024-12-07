94047d45e15df99d03605b82a4708cc735fe99ed
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-07 08:41:02 +0330

    chore(element): remove base element
    Co-authored-by: arashagp <arash.qardashpoor@gmail.com>

--- packages/element/src/lib/base-element.ts
+++
@@ -1,6 +0,0 @@
-import {LitElement} from 'lit';
-
-import {LightDomMixin} from '../mixin/light-dom.js';
-import {LoggerMixin} from '../mixin/logging.js';
-
-export const BaseElement = LightDomMixin(LoggerMixin(LitElement));
