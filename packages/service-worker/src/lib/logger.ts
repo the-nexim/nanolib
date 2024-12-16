59c0359bfa37a42bac6e21520a7b39fa740d4a30
arashagp <arash.qardashpoor@gmail.com>
2024-12-16 13:48:16 +0330

    refactor(logger): simplify logger initialization by removing package tracer and package details

--- packages/service-worker/src/lib/logger.ts
+++ packages/service-worker/src/lib/logger.ts
@@ -1,5 +1,3 @@
-import {createLogger} from '@alwatr/logger';
-import {packageTracer} from '@alwatr/package-tracer';
+import { createLogger } from '@alwatr/logger';
 
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
-export const logger = /* @__PURE__ */ createLogger(`${__package_name__}:service-worker`);
+export const logger = /* @__PURE__ */ createLogger('service-worker');
