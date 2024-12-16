21d82da0bf1191a4603c12cc1e0b67cc3be1dba1
arashagp <arash.qardashpoor@gmail.com>
2024-12-16 12:04:28 +0330

    feat(service-worker): add beforeinstallprompt option and integrate snackbar package

--- packages/service-worker/src/main.ts
+++ packages/service-worker/src/main.ts
@@ -1,3 +1,8 @@
+import {packageTracer} from '@alwatr/package-tracer';
+
+// Ensure only single version of this package use in the project (only check in development environment).
+__dev_mode__: packageTracer.add(__package_name__, __package_version__);
+
 export * from './lib/service-worker-notify.js';
 export * from './lib/service-worker.js';
 export * from './lib/install-pwa.js';
