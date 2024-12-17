509fcebdace63fcfa39b913bafde8bf2d2a2b54d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-17 12:07:19 +0330

    chore: remove snackbar package and move to nexim design system

--- packages/snackbar/src/main.ts
+++
@@ -1,6 +0,0 @@
-import {packageTracer} from '@alwatr/package-tracer';
-
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
-
-export * from './lib/element.js';
-export {snackbarSignal} from './lib/signal.js';
