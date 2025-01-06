68050d2ddc466a771876ad6bf703d2a55306bf55
arashagp <arash.qardashpoor@gmail.com>
2025-01-06 14:42:43 +0330

    docs(financial-calculate): enhance README and add detailed API documentation

--- packages/financial-calculate/README.md
+++ packages/financial-calculate/README.md
@@ -1,1 +1,47 @@
 # @nexim/financial-calculate
+
+![NPM Version](https://img.shields.io/npm/v/@nexim/financial-calculate)
+![npm bundle size](https://img.shields.io/bundlephobia/min/@nexim/financial-calculate)
+![Build & Lint & Test](https://github.com/the-nexim/nanolib/actions/workflows/build-lint-test.yaml/badge.svg)
+![NPM Downloads](https://img.shields.io/npm/dm/@nexim/financial-calculate)
+![NPM License](https://img.shields.io/npm/l/@nexim/financial-calculate)
+
+## Overview
+
+a library for performing various financial calculations. It provides a set of functions to handle common financial operations such as interest calculations, loan payments, and more.
+
+## Installation
+
+To install the package, use npm or yarn:
+
+```sh
+npm install @nexim/alpine
+
+# Or using yarn
+yarn add @nexim/alpine
+```
+
+## Api
+
+### calcPriceFromDiscount
+
+Calculate the price after applying a discount.
+
+```ts
+// with default decimal
+calcPriceFromDiscount(100, 10); // returns 90.00
+
+// with incoming decimal
+calcPriceFromDiscount(95, 10, 1); // returns 85.50
+```
+
+### calcDiscountPrice
+
+Calculate the discount amount from the original price.
+
+```ts
+// with default decimal
+calcDiscountPrice(100, 10); // returns 10.00
+// with incoming decimal
+calcDiscountPrice(95, 10, 1); // returns 9.50
+```
--- packages/financial-calculate/src/lib/calculate.ts
+++ packages/financial-calculate/src/lib/calculate.ts
@@ -1,9 +1,45 @@
-export const calcPriceFromDiscount = (price: number, discount: number, decimal = 2) => {
-  decimal = Math.pow(10, decimal);
-  return Math.round(price * (1 - discount / 100) * decimal) / decimal;
+import {createLogger} from '@alwatr/logger';
+
+const logger = createLogger(__package_name__);
+
+/**
+ * Calculate the price after applying a discount.
+ *
+ * @param {number} price - The original price.
+ * @param {number} discount - The discount percentage to apply.
+ * @param {number} [decimal=2] - The number of decimal places to round to.
+ * @returns {number} - The price after discount.
+ *
+ * @example
+ * // with default decimal
+ * calcPriceFromDiscount(100, 10); // returns 90.00
+ *
+ * // with incoming decimal
+ * calcPriceFromDiscount(95, 10, 1); // returns 85.50
+ */
+export const calcPriceFromDiscount = (price: number, discount: number, decimal = 2): number => {
+  logger.logMethodArgs?.('calcPriceFromDiscount', {price, discount, decimal});
+  const factor = Math.pow(10, decimal);
+  return Math.round(price * (1 - discount / 100) * factor) / factor;
 };
 
-export function calcDiscountPrice(price: number, discount: number, decimal = 2) {
-  decimal = Math.pow(10, decimal);
-  return Math.round(((price * discount) / 100) * decimal) / decimal;
+/**
+ * Calculate the discount amount from the original price.
+ *
+ * @param {number} price - The original price.
+ * @param {number} discount - The discount percentage.
+ * @param {number} [decimal=2] - The number of decimal places to round to.
+ * @returns {number} - The discount amount.
+ *
+ * @example
+ * // with default decimal
+ * calcDiscountPrice(100, 10); // returns 10.00
+ *
+* // with incoming decimal
+ * calcDiscountPrice(95, 10, 1); // returns 9.50
+ */
+export function calcDiscountPrice(price: number, discount: number, decimal = 2): number {
+  logger.logMethodArgs?.('calcDiscountPrice', {price, discount, decimal});
+  const factor = Math.pow(10, decimal);
+  return Math.round(((price * discount) / 100) * factor) / factor;
 }
