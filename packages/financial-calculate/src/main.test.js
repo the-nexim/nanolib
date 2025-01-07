865c16c3929d81d9b248a7b7b3b4e09da32bbebe
arashagp <arash.qardashpoor@gmail.com>
2025-01-07 08:44:19 +0330

    feat(financial-calculating): write test

--- packages/financial-calculate/src/main.test.js
+++ packages/financial-calculate/src/main.test.js
@@ -1,6 +1,15 @@
 import test from 'ava';
+import { calcDiscountFromPrice, calcPriceFromDiscount } from '@nexim/financial-calculate';
 
 // empty test
 test('empty test', (test) => {
   test.pass();
 });
+
+test('calculate discount from price', (test) => {
+  test.is(calcDiscountFromPrice(3, 4), 0.12);
+})
+
+test('calculate price from discount', (test) => {
+  test.is(calcPriceFromDiscount(3, 4), 2.88);
+})
