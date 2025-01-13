d4c10e52b4086b0e3e8d8cfc1491dd5751518cb0
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2025-01-13 12:45:10 +0330

    chore: additional comment

--- packages/element/src/main.ts
+++ packages/element/src/main.ts
@@ -1,6 +1,5 @@
 import {packageTracer} from '@alwatr/package-tracer';
 
-// Ensure only single version of this package use in the project (only check in development environment).
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
 
 export * from './mixin/light-dom.js';
