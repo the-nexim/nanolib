e4587570e202569ef1f7b06aab2d5bdf9a66bb8f
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2025-01-22 12:37:20 +0330

    refactor(eslint)!: migrate to eslint v9 flat config
    Co-authored-by: arashagp <arash.qardashpoor@gmail.com>

---
+++ packages/eslint-config/src/lib/javascript-config.ts
@@ -0,0 +1,23 @@
+import * as globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export const jsConfigs = tseslint.config({
+  extends: [ tseslint.configs.disableTypeChecked ],
+  files: [ '*.{mjs,js,cjs}', '**/*.{mjs,js,cjs}' ],
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+  },
+  languageOptions: {
+    globals: {
+      ...globals.commonjs,
+      ...globals.node,
+    },
+    sourceType: 'commonjs',
+  },
+  rules: {
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    'no-undef': 'off',
+  },
+});
---
+++ packages/eslint-config/src/main.ts
@@ -0,0 +1,3 @@
+export * from './lib/typescript-config.js';
+export * from './lib/javascript-config.js';
+export * from './lib/extended-config.js';
