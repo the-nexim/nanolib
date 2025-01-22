3e971c4406b8deee6100e3f3d7d58a0d5a962a13
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2025-01-22 12:56:41 +0330

    chore: better handle ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 const ignorePatterns = includeIgnoreFile(gitignorePath).ignores;
+ignorePatterns?.push('.yarn');
 
 export default [
   ...baseExtends,
   ...tsConfig,
   ...jsConfigs,
   {
-    ignores: Array.isArray(ignorePatterns) ? [ ...ignorePatterns, '.yarn' ] : [ './README.md' ],
+    ignores: ignorePatterns,
   },
 ];
